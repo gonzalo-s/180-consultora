65cc22647bffab00b5cdb89550da7ed8bdaa2b30
Gonzalo Soler <gonzalosoler@gmail.com>
2021-05-31 14:55:50 -0300

    Issue : Setup the Firebase project #4 - firebase initialized

--- pages/firebase.js
+++ pages/firebase.js
@@ -25,9 +25,8 @@ const firebaseConfig = {
 
 // TODO: Replace the following with your app's Firebase project configuration
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+// Initialize Firebase
 
-  
-  // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
   
   
